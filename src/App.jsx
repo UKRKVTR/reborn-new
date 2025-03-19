7b92ed43cb8a279b4b05f94c8ffa9fbc019c657e
Danylo <warfais6@gmail.com>
2025-03-19 16:41:05 +0200

    Add ItemList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import ItemList from './components/itemList'
 import Nav from './components/navigation'
 import Home from './pages/home'
 
 function App () {
   return (
     <div>
-      <Nav /> <Home />
+      <Nav /> <Home /> <ItemList />
     </div>
   )
 }
---
+++ src/components/itemList/index.jsx
@@ -0,0 +1,26 @@
+import './style.css'
+import React from 'react'
+
+function ItemList () {
+  return (
+    <div className='items'>
+      <a href='' className='itemLink'>
+        <img src='/футболка.png' alt='' className='item' />
+        <p>RE|DEMONS</p>
+        <p>900 ₴</p>
+      </a>
+      <a href='' className='itemLink'>
+        <img src='/футболка.png' alt='' className='item' />
+        <p>RE|DEMONS</p>
+        <p>900 ₴</p>
+      </a>
+      <a href='' className='itemLink'>
+        <img src='/футболка.png' alt='' className='item' />
+        <p>RE|DEMONS</p>
+        <p>900 ₴</p>
+      </a>
+    </div>
+  )
+}
+
+export default ItemList
